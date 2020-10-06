4a76681c8db269d9fce83b34e5a53d8191cf75aa
Jeff <jeff.neet@gmail.com>
2020-10-06 13:34:19 -0600

    Pulled in currencies list data to create dropdown button items

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +8,23 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropdownItems() {
+    List<DropdownMenuItem<String>> items = [];
+
+    for (String currency in currenciesList) {
+      DropdownMenuItem<String> item = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+
+      items.add(item);
+    }
+
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +61,16 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropdownItems(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                });
+                print(value);
+              },
+            ),
           ),
         ],
       ),
